3ffeac06f5f3b47f9acae2e5cac4fa20df0b0ea3
Dale Stewart <railsudemydale>
2016-11-01 13:56:35 +0000

    Add validations to Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  
   def create
     @article = Article.new(article_params)
     @article.save
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
   
-  private 
+  private
   
     def article_params
       params.require(:article).permit(:title, :body)
