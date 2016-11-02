b4bc3ad7528c40e477bcb7e8830efafc82eaa0f9
Dale Stewart <railsudemydale>
2016-11-02 22:16:30 +0000

    Handle article not found error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,16 +10,24 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:success] = "Article has been created"
+       flash[:success] = "Article has been created"
        redirect_to articles_path
-     else
-       flash.now[:danger] = "Article has not been created"
-       render :new
-     end
+    else
+    flash.now[:danger] = "Article has not been created"
+    render :new
+    end
   end
   
   def show
     @article = Article.find(params[:id])
+  end
+  
+  protected 
+  
+  def resource_not_found
+    message = "The article you are looking for could not be found"
+    flash[:alert] = message
+    redirect_to root_path
   end
   
   private
