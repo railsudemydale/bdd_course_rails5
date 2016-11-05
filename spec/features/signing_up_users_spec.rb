3365fb461d86fd7adf6cefe6aff490f672581022
Dale Stewart <railsudemydale>
2016-11-05 15:42:29 +0000

    Setup user sign in

--- spec/features/signing_up_users_spec.rb
+++ spec/features/signing_up_users_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Signup users" do
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     
-    expect(page).to have_content("You have signed up succcessfully.")
+    expect(page).to have_content("You have signed up successfully.")
   end
   
   scenario "with invalid credentials" do
@@ -20,7 +20,7 @@ RSpec.feature "Signup users" do
     fill_in "Password confirmation", with: ""
     click_button "Sign up"
     
-    #expect(page).to have_content("You have not signed up succcessfully.")
+    #expect(page).to have_content("You have not signed up successfully.")
   end
 end
 
